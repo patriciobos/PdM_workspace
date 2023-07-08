02afae52acb683e672a33ada01d45949062f831b
sbualo <sbualo@itba.edu.ar>
2023-07-08 10:20:56 -0300

    Practica 3 Finalizada

---
+++ Programación de Microcontroladores/Practica 3/Drivers/API/Src/API_delay.c
@@ -0,0 +1,47 @@
+/*
+ * API_delay.c
+ *
+ *  Created on: Jul 6, 2023
+ *      Author: santiagobualo
+ */
+
+#include "API_delay.h"
+
+
+
+void delayInit( delay_t * delay, tick_t duration ){ //Setea el tiempo pero no inicializa el tim
+
+	if (delay == NULL || duration <=0){
+		Error_Handler();
+	}
+	delay->running = false;
+	delay->duration = duration;
+
+}
+
+bool_t delayRead( delay_t * delay ){
+	if (delay == NULL){
+			Error_Handler();
+		}
+	if (delay->running == false){ 				//No estaba inicializado
+		delay->startTime = HAL_GetTick();
+		delay->running = true;	//Empueza a correr
+		return false;
+	}else{
+		if( HAL_GetTick() - delay->startTime > delay->duration){
+			//Termino el tiempo
+			delay->running = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
+
+void delayWrite( delay_t * delay, tick_t duration ){
+	if (delay == NULL || duration <=0){
+			Error_Handler();
+		}
+	delay->duration = duration;
+}
